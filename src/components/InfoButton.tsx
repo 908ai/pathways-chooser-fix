0e2ecfe66ae0b05ec70664387d44715760d944dd
[dyad] <esilva12@gmail.com>
2025-09-16 10:26:52 -0600

    [dyad] Adjusted info button sizing - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/InfoButton.tsx
+++ src/components/InfoButton.tsx
@@ -23,8 +23,8 @@ const InfoButton: React.FC<InfoButtonProps> = ({ title, children, dialogClassNam
             <Tooltip>
                 <TooltipTrigger asChild>
                     <DialogTrigger asChild>
-                        <Button variant="ghost" size="icon" className="text-blue-400 hover:bg-blue-500/20">
-                            <Info className="h-6 w-6" />
+                        <Button variant="ghost" size="icon" className="text-blue-400 hover:bg-blue-500/20 h-6 w-6">
+                            <Info />
                         </Button>
                     </DialogTrigger>
                 </TooltipTrigger>
