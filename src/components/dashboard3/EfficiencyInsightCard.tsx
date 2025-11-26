f0c6ce3f395e9012f706c93285fee42137791461
[dyad] <esilva12@gmail.com>
2025-11-26 12:44:55 -0700

    [dyad] Adjusted font size on insight card - wrote 5 file(s) + extra files edited outside of Dyad

--- src/components/dashboard3/EfficiencyInsightCard.tsx
+++ src/components/dashboard3/EfficiencyInsightCard.tsx
@@ -115,7 +115,7 @@ const EfficiencyInsightCard = () => {
             </svg>
             <div className="absolute inset-0 flex flex-col items-center justify-center">
               <span className="text-3xl font-bold">{currentInsight.progress.value}%</span>
-              <span className="text-xs text-center">{currentInsight.progress.label}</span>
+              <span className="text-[10px] text-center uppercase tracking-wider">{currentInsight.progress.label}</span>
             </div>
           </div>
         </div>
